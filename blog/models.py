c17cf96bd30251a8d4fb23b152b845e0eedeeb4b
johnvas05 <moberdog2005@gmail.com>
2025-06-26 20:29:42 +0300

    initial commit

---
+++ blog/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'P', 'Published'
+
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+
+    class Meta:
+        ordering = ['-publish']
+
+    def __str__(self):
+        return self.title
