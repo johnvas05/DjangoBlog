b772e600c0c3035d3c51fe4ba8b6bbee373adea9
johnvas05 <moberdog2005@gmail.com>
2025-07-02 00:19:42 +0300

    fixed views.py

--- blog/views.py
+++ blog/views.py
@@ -8,4 +8,4 @@ def post_list(request):
 
 def post_detail(request, id):
     post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
-    return render(request, 'blog/post/detail.html', {'posts': posts})
+    return render(request, 'blog/post/detail.html', {'posts': post})
