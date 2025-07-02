88505ba3e5f16dc09177d78a3a65123dd4a29777
johnvas05 <moberdog2005@gmail.com>
2025-07-02 11:20:38 +0300

    added absolute url functionality

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
+from django.urls import reverse
 
 class Post(models.Model):
     class Status(models.TextChoices):
@@ -21,3 +22,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.id])
