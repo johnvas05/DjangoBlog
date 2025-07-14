6a8382c15c4d286c1cf517218bab30d0d1d9ae0d
johnvas05 <moberdog2005@gmail.com>
2025-07-14 22:35:48 +0300

    moved from django-tailwind to tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./templates/**/*.html",
+    "./blog/templates/**/*.html",
+    "./account/templates/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
